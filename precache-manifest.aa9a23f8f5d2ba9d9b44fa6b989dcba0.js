07f1911288d4b1cf6b3c1a6fd00819041f50d7b2
Pranav Mahajan <mahajanpranav41@gmail.com>
2020-08-17 12:49:19 +0530

    Updates

---
+++ precache-manifest.aa9a23f8f5d2ba9d9b44fa6b989dcba0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09835d6d454ce5f078ca8220f04d7670",
+    "url": "/covid-tracker/index.html"
+  },
+  {
+    "revision": "2c4029d960c4114672b1",
+    "url": "/covid-tracker/static/css/main.cce86fe2.chunk.css"
+  },
+  {
+    "revision": "23abf1d7f3143f5a1227",
+    "url": "/covid-tracker/static/js/2.09320ebb.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/covid-tracker/static/js/2.09320ebb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c4029d960c4114672b1",
+    "url": "/covid-tracker/static/js/main.5fc94f9f.chunk.js"
+  },
+  {
+    "revision": "18d13f8c521942df29a2",
+    "url": "/covid-tracker/static/js/runtime-main.e6ffcc43.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-tracker/static/media/image.d7265326.png"
+  }
+]);
